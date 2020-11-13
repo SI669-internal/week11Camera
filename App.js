79d3ea49f73aae53b06f83d67374dc548968c22e
Mark Newman <mwnewman@umich.edu>
2020-11-13 01:59:37 -0500

    first and final

--- App.js
+++ App.js
@@ -1,21 +1,167 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+import { FontAwesome } from '@expo/vector-icons'; 
+import { getDataModel } from './DataModel';
 
-export default function App() {
+class ImageTest extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require('./images/ImageNotAvailable.png')}
+        />
+        <Image
+          style={styles.logo}
+          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+        />
+      </View>
+    );
+  }
+}
+
+class MainScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.dataModel = getDataModel();
+    this.dataModel.subscribeToImageUpdate(this.onImageUpdate);
+
+    this.state = {
+      theImage: require('./images/ImageNotAvailable.png')
+    }
+  }
+
+  onImageUpdate = (imageObject) => {
+    this.setState({
+      theImage: imageObject
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.mainImage}
+          source={this.state.theImage}
+        />
+        <Button
+          title="Take a Picture!"
+          onPress={()=>{
+            this.props.navigation.navigate('Camera');
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+class CameraScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.dataModel = getDataModel();
+
+    this.state= {
+      hasCameraPermission: false,
+    }
+  }
+
+  componentDidMount() {
+    this.getPermissions();
+  }
+
+  getPermissions = async () => {
+    let cameraPerms = await Permissions.askAsync(Permissions.CAMERA);
+    let permGranted = cameraPerms.status === 'granted';
+    this.setState({
+      hasCameraPermission: permGranted
+    });
+  }
+
+  onTakePicture = async () => {
+    let picData = await this.cameraRef.takePictureAsync();
+    this.dataModel.updateImage(picData);
+    console.log('took picture!', picData);
+    this.props.navigation.goBack();
+  }
+
+  render() {
+    return (
+      <View style={styles.cameraContainer}>
+        <Camera 
+          style={styles.camera}
+          ratio='4:3'
+          pictureSize='Medium'
+          ref={ref=>this.cameraRef=ref}
+        />
+        <TouchableOpacity 
+          style={styles.cameraControls}
+          onPress={this.onTakePicture}>
+          <Text style={styles.snapText}>Snap!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+
+
+const Stack = createStackNavigator();
+
+function Nav() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Main"   
+      >
+        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+
 const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+  },
+  camera: {
+    flex: 0.85,
+  },
+  cameraControls: {
+    flex: 0.1, 
+    justifyContent: 'flex-start', 
+    alignItems: 'center',
+    padding: '5%',
+    width: '100%',
+    backgroundColor: '#222'
+  },
+  snapText: {
+    fontSize: 36,
+    color: 'white'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
+  },  
+  mainImage: {
+    height: 400,
+    width: 300,
+    resizeMode: 'contain'
+  },
+  logo: {
+    width: 400,
+    height: 100,
+    resizeMode: 'contain'
   },
 });
+
+export default Nav;
---
+++ DataModel.js
@@ -0,0 +1,67 @@
+
+import firebase from 'firebase';
+import '@firebase/firestore';
+import '@firebase/storage';
+import { firebaseConfig } from './Secrets';
+
+class DataModel {
+  constructor() {
+    // connect to firebase and get refs
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    this.currentImageRef = firebase.firestore().doc('images/currentImage');
+    this.storageRef = firebase.storage().ref();
+
+    this.theImage = undefined;
+    this.theCallback = undefined;
+    this.loadImage();
+  }
+
+  loadImage = async () => {
+    let imageDocSnap = await this.currentImageRef.get();
+    this.theImage = imageDocSnap.data();
+    console.log("got image info", this.theImage);
+    if (this.theCallback) {
+      this.theCallback(this.theImage);
+    }
+  }
+
+  subscribeToImageUpdate = (callback) => {
+    this.theCallback = callback;
+  }
+
+  updateImage = async (imageObject) => {
+    //imageObject format: {uri: xxx, width: yyy, height: zzz}
+    this.theImage = imageObject;
+
+    // Set up storage refs and download URL
+    let fileName = '' + Date.now();
+    let imageRef = this.storageRef.child(fileName);
+    
+    // fetch the image object from the local filesystem
+    let response = await fetch(imageObject.uri);
+    let imageBlob = await response.blob();
+
+    // then upload it to Firebase Storage
+    await imageRef.put(imageBlob);
+
+    // // ... and update the current image Document in Firestore
+    let downloadURL = await imageRef.getDownloadURL();
+    imageObject.uri = downloadURL; // replace local URI with permanent one
+    await this.currentImageRef.set(imageObject);
+
+    if (this.theCallback) {
+      this.theCallback(imageObject);
+    }
+  }
+}
+
+let theDataModel = undefined;
+
+export function getDataModel() {
+  if (!theDataModel) {
+    theDataModel = new DataModel();
+  }
+  return theDataModel;
+}
